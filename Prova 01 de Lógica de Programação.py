8f6de31b6dccd5130dcf795b52e4277345f75820
romulovieira777 <61069548+romulovieira777@users.noreply.github.com>
2020-04-14 17:21:45 -0300

    Prova 01 de Lógica de Programação

---
+++ Prova 01 de Lógica de Programação.py
@@ -0,0 +1,132 @@
+#Prova 01 de Lógica de Programação
+
+#Pergunta 01
+'''
+num = 3
+num = 5
+num = num-num
+print(num)
+'''
+
+#Pergunta 02
+'''
+num = -2
+num = num + num
+num = num**2
+print(num)
+'''
+
+#Pergunta 04
+'''
+a = 5 > 5
+print(a)
+b = 5 <= 5
+print(b)
+c = 5 != 5
+print(c)
+d = 5 != 10
+print(d)
+e = '5' != 5
+print(e)
+f = 100.0 == 100
+print(f)
+g = 5 == '5'
+print(g)
+
+print(a,b,c,d,e,f,g)
+'''
+
+#Pergunta 05
+'''
+a = 8 < 10
+print(a)
+b = '8' < '10'
+print(b)
+c = 10 != 8
+print(c)
+d = 10 >= 8
+print(d)
+e = 10 == 8
+print(e)
+f = '81' < '9'
+print(f)
+g = '777' != "777"
+print(g)
+print(a,b,c,d,e,f,g)
+'''
+
+#Pergunta 06
+'''
+a = 'mel' < 'lucas'
+print(a)
+b = 'ana' == "ana"
+print(b)
+c = 'LuCaS' != 'LUCas'
+print(c)
+d = 'amanda' > "AMANDA"
+print(d)
+e = 'MeL' >= 'MEL'
+print(e)
+f = 'Lucas' < 'Mel'
+print(e)
+g = 'Mel' <= 'Mel'
+print(g)
+
+print(a,b,c,d,e,f,g)
+'''
+
+#Pergunta 07
+
+'''
+num = 10
+num = num + 5
+num == 20
+num = num + 1
+print(num)
+'''
+
+#Pergunta 08
+'''
+num = 10
+num = num + 5
+num = num == 20
+num = num + 1
+print(num)
+'''
+
+#Pergunta 09
+'''
+a = 10 >= 8 and 5 != 3
+print(a)
+b = 10 >= 8 and 5 == 3 or 14 < 5
+print(b)
+print(a,b)
+'''
+
+#Pergunta 10
+'''
+import math
+a = 2 * 3 // 2 == (24 // 4 * 0.5)
+print(a)
+b = math.sqrt(3**2) != ((1+2)**2)**(1/2)
+print(b)
+print(a,b)
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
